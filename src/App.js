23b19b80e8c3311ed700706537d274709de7483d
Wendy Teo <wendyteo.wy@gmail.com>
2022-08-06 12:44:01 +1000

    Set up the basic skeleton for project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.pokedexTitle}>Pokedex</div>
+      <div className={styles.pokedexImage}>
+        <button>Left</button>
+        <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' alt='ditto'/>
+        <button>Right</button>
+        <h1>Ditto</h1>
+      </div>
+      <div>
+        <div className={styles.pokedexData}>Data</div>
+        <div className={styles.pokedexSearchi}>Search</div>
+      </div>
     </div>
   );
 }
